e94a3159e0606e49b90fdf722fb89c6be68441bf
bindra41048 <vsbindra@stanford.edu>
2017-04-30 13:39:41 -0700

    Links working, removed clutter, single landing page

--- page2/PageTwoController.js
+++
@@ -1,8 +0,0 @@
-
-refugeeApp.controller('PageTwoController', ['$scope', '$routeParams', '$resource',
-    function ($scope, $routeParams, $resource) {
-        $scope.pageTwo = {};
-        $scope.pageTwo.showAlert = function() {
-            window.alert("Alert from page two");
-        };
-    }]);
--- page3/PageThreeController.js
+++
@@ -1,7 +0,0 @@
-refugeeApp.controller('PageThreeController', ['$scope', '$routeParams', '$resource',
-    function ($scope, $routeParams, $resource) {
-        $scope.pageThree = {};
-        $scope.pageThree.showAlert = function() {
-            window.alert("Alert from page three");
-        };
-    }]);
